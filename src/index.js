fef756a16023a4b50cc2b1348ac92c9f0cf2e006
ChenXiaoyao <chenxiaoyao6228@163.com>
2019-10-26 23:06:07 +0800

    feat: add setAttr to element

--- src/index.js
+++ src/index.js
@@ -8,9 +8,26 @@ ReactDom.render(
   React.createElement(
     'ul',
     { className: 'list' },
-    React.createElement('li', { className: 'item' }, 'item'),
-    React.createElement('li', { className: 'item' }, 'item'),
-    React.createElement('li', { className: 'item' }, 'item')
+    React.createElement(
+      'li',
+      { className: 'item', style: 'background: red' },
+      'item'
+    ),
+    React.createElement(
+      'li',
+      { className: 'item', style: { color: 'blue' } },
+      'item'
+    ),
+    React.createElement(
+      'li',
+      {
+        className: 'item',
+        onClick: function() {
+          window.alert('触发点击事件')
+        }
+      },
+      'item'
+    )
   ),
   root
 )
--- src/react-dom.js
+++ src/react-dom.js
@@ -1,9 +1,14 @@
+import * as utils from './utils'
+
 function render(vdom, rootElement) {
   // root节点相对特殊, 因此单独处理
   rootElement.appendChild(_render(vdom))
   function _render(vdom) {
     let { tagName, attrs, children = [] } = vdom
     let element = document.createElement(tagName)
+    for (let key in attrs) {
+      utils.setAttr(element, key, attrs[key])
+    }
     for (let child of children) {
       let childElement
       if (typeof child === 'string') {
