eff6ae8a91d0a71b903eec536152a50c44bc7400
ChenXiaoyao <chenxiaoyao6228@163.com>
2019-11-17 16:28:55 +0800

    add basic structure

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('link');
+let path = require('path');
+
+let config = require(path.resolve(process.cwd(), 'webpack.config.js'));
+console.log(config);
+
+let Compiler = require('../lib/Compiler.js');
+
+let compiler = new Compiler(config);
+
+compiler.run();
